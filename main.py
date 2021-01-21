7a32e388abf25c3c6f6e617d9d16499d8a561303
AtlasR2D2 <75536318+AtlasR2D2@users.noreply.github.com>
2021-01-21 22:03:00 +0000

    Removed example coffees variable initialisation after generate_stars() definition

--- main.py
+++ main.py
@@ -25,9 +25,6 @@ def generate_stars(star_icon, num: int):
     # Output String
     return star_string
 
-
-coffees = [generate_stars(COFFEE_EMOJI, num) for num in range(MAX_STARS+1)]
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
